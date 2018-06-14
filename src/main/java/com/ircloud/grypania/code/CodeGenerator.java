f673509940ab5fb4ee25b346fd9522634d045ca7
陈业镜 <chenyj@77ircloud.com>
2018-06-14 14:39:19 +0800

    init

---
+++ src/main/java/com/ircloud/grypania/code/CodeGenerator.java
@@ -0,0 +1,76 @@
+package com.ircloud.grypania.code;
+
+import com.baomidou.mybatisplus.enums.IdType;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.TemplateConfig;
+import com.baomidou.mybatisplus.generator.config.rules.DbType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+
+/**
+ * @author: Born
+ * @create: 2018/6/13
+ **/
+public class CodeGenerator {
+
+    /**
+     * <p>
+     * MySQL 生成演示
+     * </p>
+     */
+    public static void main(String[] args) {
+        AutoGenerator mpg = new AutoGenerator();
+
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        gc.setOutputDir("code");
+        gc.setFileOverride(true);// 文件是否覆盖
+        gc.setActiveRecord(false);// 开启 ActiveRecord 模式
+        gc.setEnableCache(false);// XML 二级缓存
+        gc.setBaseResultMap(false);// XML ResultMap
+        gc.setBaseColumnList(false);// XML columList
+        gc.setAuthor("Bron");
+        gc.setIdType(IdType.ID_WORKER);
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setDbType(DbType.MYSQL);
+        dsc.setDriverName("com.mysql.cj.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("root");
+        dsc.setUrl("jdbc:mysql://192.168.9.172:3306/user?characterEncoding=utf8");
+        mpg.setDataSource(dsc);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setTablePrefix(new String[]{"t_"});// 此处可以修改为您的表前缀
+        strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
+        strategy.setEntityLombokModel(true);
+        strategy.setLogicDeleteFieldName("fdelete");
+        strategy.entityTableFieldAnnotationEnable(true);
+        strategy.setEntityColumnConstant(true);
+        strategy.setInclude(new String[]{"t_user"}); // 需要生成的表
+        mpg.setStrategy(strategy);
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setEntity("model");
+        pc.setParent("com.rttx.scaffold");
+        pc.setModuleName("dao");
+        mpg.setPackageInfo(pc);
+
+        // 关闭默认 xml 生成，调整生成 至 根目录
+        TemplateConfig tc = new TemplateConfig();
+        tc.setController(null);
+        tc.setService(null);
+        tc.setServiceImpl(null);
+        mpg.setTemplate(tc);
+
+        // 执行生成
+        mpg.execute();
+    }
+}
